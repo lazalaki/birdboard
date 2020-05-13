c4871f81f9517d114e49f690eeadf2b3be6417ad
lazalaki <lakierbes@gmail.com>
2020-05-13 17:23:51 +0200

    lession 32 finished

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,15 +2,34 @@
 
 @section('content')
 
-    <header class="flex items-center mb-3 py-4">
+    <header class="flex items-center mb-3 pb-4">
 
         <div class="flex justify-between w-full items-end">
             
-            <p class="text-gray-500 text-base font-normal">
+            <p class="text-gray-500 text-sm font-normal">
                 <a href="/projects">My projects</a> / {{ $project->title }}
             </p>
             
-            <a href="{{ $project->path() . '/edit'}}" class="button">Edit Project</a>
+            <div class="flex items-center">
+                @foreach ($project->members as $member)
+                    
+                    <img 
+                        src="{{ gravatar_url($member->email) }}" 
+                        alt="{{ $member->name }}'s avatar" 
+                        class="rounded-full inline w-8 mr-2"
+                    >
+
+                @endforeach
+
+                <img 
+                    src="{{ gravatar_url($project->owner->email) }}" 
+                    alt="{{ $project->owner->name }}'s avatar" 
+                    class="rounded-full inline w-8 mr-2"
+                >
+
+
+                <a href="{{ $project->path() . '/edit'}}" class="button ml-4">Edit Project</a>
+            </div>
 
         </div> 
 
