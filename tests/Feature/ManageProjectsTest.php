7435d7f05c789f4c6da45ceee5d244587e65d02f
lazalaki <lakierbes@gmail.com>
2020-05-19 16:52:19 +0200

    lession 35 finished

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -42,21 +42,12 @@ class ManageProjectsTest extends TestCase
 
         $this->get('projects/create')->assertStatus(200);
 
-        $attributes = [
-            'title' => $this->faker->sentence(),
-            'description' => $this->faker->sentence,
-            'notes' => 'General notes here.'
-        ]; 
+        $attributes = factory(Project::class)->raw();
 
-        $response = $this->post('/projects', $attributes);
+        $response = $this->followingRedirects()->post('/projects', $attributes);
 
-        $project = Project::where($attributes)->first();
 
-        $response->assertRedirect($project->path());
-
-        $this->assertDatabaseHas('projects', $attributes);
-
-        $this->get($project->path())
+        $response
         ->assertSee($attributes['title'])
         ->assertSee($attributes['description'])
         ->assertSee($attributes['notes']);
@@ -74,9 +65,13 @@ class ManageProjectsTest extends TestCase
             $this->delete($project->path())
                  ->assertRedirect('/login');
     
-            $this->signIn();
+            $user = $this->signIn();
 
             $this->delete($project->path())->assertStatus(403);
+
+            $project->invite($user);
+
+            $this->actingAs($user)->delete($project->path())->assertStatus(403);
         }
 
 
